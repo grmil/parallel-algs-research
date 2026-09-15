5c483c169c0e808d264ff49712dcd5eb9a6f9ff6
Grady Miller <182833936+gradymiller@users.noreply.github.com>
2026-09-15 11:57:37 -0400

    added iterator capabilities to vector, added more arithmetic ops, added dot products

--- include/serial/vector.h
+++ include/serial/vector.h
@@ -4,89 +4,127 @@
 #include <cstddef>
 
 template <typename T = double>
-class Array {
+class Vector {
     std::vector<T> data;
 
-public:
-    Array() = default;
-    explicit Array(std::size_t n) : data(n) {}
-    Array(std::initializer_list<T> init) : data(init) {}
+    void checkSameSize(const Vector<T>& rhs) const {
+        if (data.size() != rhs.data.size())
+            throw std::invalid_argument("Vector size mismatch");
+    }
 
+public:
+    Vector() = default;
+    explicit Vector(std::size_t n) : data(n) {}
+    Vector(std::initializer_list<T> init) : data(init) {}
+
+    // Size, piped from std::vector
     std::size_t size() const { return data.size(); }
 
     // Item access operator definitions
     T& operator[](std::size_t i) { return data[i]; }
     const T& operator[](std::size_t i) const { return data[i]; }
 
-    T& operato
-};
+    // Iterators for loops
+    auto begin() { return data.begin(); }
+    auto end()   { return data.end(); }
+    auto begin() const { return data.begin(); }
+    auto end()   const { return data.end(); }
 
-template <typename T>
-Array<T> operator+(const Array<T>& a, const Array<T>& b) {
-    if (a.size() != b.size()) {
-        throw std::invalid_argument("Array size mismatch");
+    // Inner Product
+    T dot(const Vector<T>& rhs) const {
+        checkSameSize(rhs);
+        T result{};
+        for (std::size_t i = 0; i < size(); ++i) {
+            result += data[i] * rhs[i];
+        }
+        return result;
     }
 
-    Array<T> result(a.size());
-    for (std::size_t i = 0; i < a.size(); ++i) {
-        result[i] = a[i] + b[i];
+    // Compound assignment - vector, vector
+    Vector<T>& operator+=(const Vector<T>& rhs) {
+        checkSameSize(rhs);
+        for (std::size_t i = 0; i < data.size(); ++i)
+            data[i] += rhs.data[i];
+        return *this;
     }
 
-    return result;
-};
-
-template <typename T>
-Array<T> operator-(const Array<T>& a, const Array<T>& b) {
-  if (a.size() != b.size()) {
-    throw std::invalid_argument("Array size mismatch");
-  }
-
-  Array<T> result(a.size());
-  for (std::size_t i = 0; i < a.size(); ++i) {
-    result[i] = a[i] - b[i];
-  }
-
-  return result;
-}
-
-
-template <typename T>
-Array<T> operator*(const Array<T>& a, const Array<T>& b) {
-  if (a.size() != b.size()) {
-    throw std::invalid_argument("Array size mismatch");
-  }
-
-  Array<T> result(a.size());
-  for (std::size_t i = 0; i < a.size(); ++i) {
-    result[i] = a[i] * b[i];
-  }
-
-  return result;
-}
-
-template <typename T>
-Array<T> operator/(const Array<T>& a, const Array<T>& b) {
-  if (a.size() != b.size()) {
-    throw std::invalid_argument("Array size mismatch");
-  }
-
-  Array<T> result(a.size());
-  for (std::size_t i = 0; i < a.size(); ++i) {
-    result[i] = a[i] / b[i];
-  }
-
-  return result;
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const Array<T>& v) {
-  os << "[";
-  for (std::size_t i = 0; i < v.size(); ++i) {
-    os << v[i];
-    if (i != v.size() - 1) {
-      os << ", ";
+    Vector<T>& operator-=(const Vector<T>& rhs) {
+        checkSameSize(rhs);
+        for (std::size_t i = 0; i < data.size(); ++i)
+            data[i] -= rhs.data[i];
+        return *this;
     }
-  }
-  os << "]";
-  return os;
+
+    Vector<T>& operator*=(const Vector<T>& rhs) {
+        checkSameSize(rhs);
+        for (std::size_t i = 0; i < data.size(); ++i)
+            data[i] *= rhs.data[i];
+        return *this;
+    }
+
+    Vector<T>& operator/=(const Vector<T>& rhs) {
+        checkSameSize(rhs);
+        for (std::size_t i = 0; i < data.size(); ++i)
+            data[i] /= rhs.data[i];
+        return *this;
+    }
+
+    // Compound assignment - vector, scalar
+    Vector<T>& operator*=(const T& scalar) {
+        for (std::size_t i = 0; i < data.size(); ++i)
+            data[i] *= scalar;
+        return *this;
+    }
 };
+
+// vector, vector
+template <typename T>
+Vector<T> operator+(Vector<T> a, const Vector<T>& b) {
+    a += b;
+    return a;
+}
+
+template <typename T>
+Vector<T> operator-(Vector<T> a, const Vector<T>& b) {
+    a -= b;
+    return a;
+}
+
+template <typename T>
+Vector<T> operator*(Vector<T> a, const Vector<T>& b) {
+    a *= b;
+    return a;
+}
+
+template <typename T>
+Vector<T> operator/(Vector<T> a, const Vector<T>& b) {
+    a /= b;
+    return a;
+}
+
+// vector, scalar and scalar, vector
+template <typename T>
+Vector<T> operator*(Vector<T> a, const T& scalar) {
+    a *= scalar;
+    return a;
+}
+
+template <typename T>
+Vector<T> operator*(const T& scalar, Vector<T> b) {
+    b *= scalar;
+    return b;
+}
+
+// Output stream
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
+    os << "[";
+    for (std::size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i != v.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
